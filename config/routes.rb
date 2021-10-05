dbb9ac4a226a9c0307baae5a84f367630c4253df
Owen Bradley <theoandtheb@gmail.com>
2021-10-05 01:17:58 -0400

    Routes cleaned up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :universities
+  resources :universities, only: [:index]
+  root :to => 'universities#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
